f525312d1da8268e460abc9cbff57b9f1be6bba3
Vishal Brahmbhatt <vdb2@njit.edu>
2022-05-10 14:33:59 -0400

    Proj p4

---
+++ tests/click_test.py
@@ -0,0 +1,18 @@
+# import logging
+import os
+# from app.db import config
+from click.testing import CliRunner
+# from pathlib import Path
+from app import create_database
+
+runner = CliRunner()
+
+
+def test_create_database():
+    response = runner.invoke(create_database)
+    assert response.exit_code == 0
+    root = os.path.dirname(os.path.abspath(__file__))
+    # set the name of the apps log folder to logs
+    dbdir = os.path.join(root, '../database')
+    # make a directory if it doesn't exist
+    assert os.path.exists(dbdir) is True
