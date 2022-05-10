9dd5acb5b8435d62671589c1d31cbe1706067104
Vishal Brahmbhatt <vdb2@njit.edu>
2022-05-09 23:52:50 -0400

    proj v1

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,3 +9,10 @@ def test_request_main_menu_links(client):
     assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index" in response.data
